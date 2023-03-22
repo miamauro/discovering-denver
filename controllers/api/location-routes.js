bf703a31106dd05034028726e9f079cd6e17aa84
Thomas Wray <twray95@gmail.com>
2023-03-22 11:31:36 -0600

    location get route specific location

--- controllers/api/location-routes.js
+++ controllers/api/location-routes.js
@@ -1,15 +1,24 @@
 const router = require("express").Router();
 const { Location } = require("../../models");
 
-router.get("/:id", async (req, res) => {
+//get all locations for a given category
+router.get("/:category", async (req, res) => {
   try {
     const locationData = await Location.findAll({
-      where: { category_id: req.params.id },
+      where: { category_id: req.params.category },
     });
     const locations = locationData.map((element) =>
       element.get({ plain: true })
     );
     res.status(200).render("locations", locations);
+  } catch (err) {
+    console.log(err);
+    res.status(500).redirect("/");
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
   } catch (err) {
     console.log(err);
     res.status(500).redirect("/");
