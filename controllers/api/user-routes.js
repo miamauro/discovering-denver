32d7e1fe1181bc6012a88e6ec11f184292374f5e
Thomas Wray <twray95@gmail.com>
2023-03-22 13:12:46 -0600

    added login route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const { User } = require("../../models");
 
 //user route to get user data and associated reviews
+//Need to add cookies check for loggedIn
 router.get("/:id", async (req, res) => {
   try {
     const userData = await User.findByPk(req.params.id, {
@@ -17,6 +18,7 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+//create new user
 router.post("/", async (req, res) => {
   try {
     if (req.body.username && req.body.email && req.body.password) {
@@ -31,5 +33,28 @@ router.post("/", async (req, res) => {
   } catch (err) {
     console.log(err);
     res.status(500).redirect("/");
+  }
+});
+
+//login
+router.post("/login", async (req, res) => {
+  try {
+    const userData = await User.findOne({
+      where: { username: req.body.username },
+    });
+    if (!userData) {
+      res.status(404).json({ message: "Incorrect username or password!" });
+      return;
+    }
+    const passwordValidate = await userData.checkPassword(req.body.password);
+    if (!passwordValidate) {
+      res.status(404).json({ message: "Incorrect username or password!" });
+      return;
+    }
+    //add cookies for loggedIn here
+    res.status(200).json({ user: userData, message: "Successfully logged in" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
   }
 });
