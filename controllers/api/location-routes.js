ea0266c568cafe8516918ec170f098ae7605cddc
Thomas Wray <twray95@gmail.com>
2023-03-22 10:07:14 -0600

    edited models

--- controllers/api/location-routes.js
+++ controllers/api/location-routes.js
@@ -1,1 +1,2 @@
 const router = require("express").Router();
+const { Location } = require("../../models");
--- models/User.js
+++ models/User.js
@@ -14,6 +14,7 @@ User.init(
     username: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
     },
     email: {
       type: DataTypes.STRING,
@@ -27,7 +28,7 @@ User.init(
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [6],
+        len: [5, 15],
       },
     },
   },
