c8b0211dcd1bb5765154bf70b2579550224da55a
Thomas Wray <twray95@gmail.com>
2023-03-22 10:52:19 -0600

    added review-routes and routing code

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,2 +1,17 @@
 const router = require("express").Router();
 const { User } = require("../../models");
+
+router.get("/:id", async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.params.id, {
+      include: [
+        { model: Review, attributes: ["review_text", "rating", "location_id"] },
+      ],
+    });
+    const user = userData.get({ plain: true });
+    res.status(200).render("user", user);
+  } catch (err) {
+    console.log(err);
+    res.status(500).redirect("/");
+  }
+});
