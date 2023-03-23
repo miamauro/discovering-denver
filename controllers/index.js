06d74e3276c7f4737644f2e4922a2c168c10bdee
George Harrison <g.a.harrison91@gmail.com>
2023-03-23 11:46:06 -0600

    pulling review title and text to homepage

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,18 @@
 const router = require("express").Router();
 const apiRoutes = require("./api/index");
+const Review = require("../models/Review");
 
 router.use("/api", apiRoutes);
 
 router.get("/", async (req, res) => {
-  res.render("homepage");
-  return;
+  const reviewData = await Review.findAll().catch((err) => {
+    res.json(err);
+  });
+  const twoReviews = reviewData.slice(reviewData.length - 2);
+  const reviews = twoReviews.map((review) => review.get({ plain: true }));
+  console.log(reviews);
+
+  res.render("homepage", { reviews });
 });
 
 module.exports = router;
