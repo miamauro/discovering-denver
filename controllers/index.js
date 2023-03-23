07e530b79fafdf66294515f5ad33bd6704c7ddf1
Thomas Wray <twray95@gmail.com>
2023-03-23 13:25:09 -0600

    locations.handlebars displays location info and reviews

--- controllers/index.js
+++ controllers/index.js
@@ -7,15 +7,4 @@ const homeRoutes = require("./homeRoutes");
 router.use("/api", apiRoutes);
 router.use("/", homeRoutes);
 
-router.get("/", async (req, res) => {
-  const reviewData = await Review.findAll().catch((err) => {
-    res.json(err);
-  });
-  const twoReviews = reviewData.slice(reviewData.length - 2);
-  const reviews = twoReviews.map((review) => review.get({ plain: true }));
-  // console.log(reviews);
-
-  res.render("homepage", { reviews });
-});
-
 module.exports = router;
