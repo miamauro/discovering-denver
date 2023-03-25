5ae7742bb1eadc0c43b4a90466208bf6ac5de317
Francisco J Gutierrez Rodarte <Francisco.GutierrezRodarte@du.edu>
2023-03-25 08:46:01 -0700

    homepage navbar responsive styling

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,9 +1,61 @@
-const ratingVal = $(".rating-value").text();
-let stars = [];
+// import React, { useState } from 'react';
+// import Button from 'react-bootstrap/Button';
+// import Collapse from 'react-bootstrap/Collapse';
 
-// if()
+// function Example() {
+//   const [open, setOpen] = useState(false);
 
-// stars.push(ratingVal);
-console.log("------------------------" + ratingVal);
+//   return (
+//     <>
+//       <Button
+//         onClick={() => setOpen(!open)}
+//         aria-controls="example-collapse-text"
+//         aria-expanded={open}
+//       >
+//         click
+//       </Button>
+//       <Collapse in={open}>
+//         <div id="example-collapse-text">
+//           Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
+//           terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+//           labore wes anderson cred nesciunt sapiente ea proident.
+//         </div>
+//       </Collapse>
+//     </>
+//   );
+// }
 
- 
+// render(<Example />);
+
+var h = $("#review-text")[0].scrollHeight;
+
+(function ($) {
+  $.fn.clickToggle = function (func1, func2) {
+    var funcs = [func1, func2];
+    this.data("toggleclicked", 0);
+    this.click(function () {
+      var data = $(this).data();
+      var tc = data.toggleclicked;
+      $.proxy(funcs[tc], this)();
+      data.toggleclicked = (tc + 1) % 2;
+    });
+    return this;
+  };
+})(jQuery);
+
+$("#more").clickToggle(
+  function () {
+    // alert("First handler: " + $(this).text());
+    $("#review-text").animate({
+      height: h,
+    });
+    $(this).text("less...");
+  },
+  function () {
+    // alert("Second handler: " + $(this).text());
+    $("#review-text").animate({
+      height: "60px",
+    });
+    $(this).text("... Read more");
+  }
+);
