85659656eea1c2c400ec5c90452eaeb39defcc22
James Geneser <jamesgeneser@gmail.com>
2023-03-25 15:54:31 -0600

    rating array appears

---
+++ utils/helpers.js
@@ -0,0 +1,17 @@
+// const calculateAverageRating = (rating) => {
+//   let ratingArr = [];
+//   ratingArr.push(rating);
+//   forEach(calculateAverageRating(rating));
+//   console.log(ratingArr);
+//   return ratingArr;
+// };
+
+module.exports = {
+  calculateAverageRating: (rating) => {
+    let ratingArr = [];
+    ratingArr.push(rating);
+    forEach(calculateAverageRating(rating));
+    console.log(ratingArr);
+    return ratingArr;
+  },
+};
