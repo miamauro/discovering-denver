be73f9bedaa980f256e6f5762705830b4fc64499
James Geneser <jamesgeneser@gmail.com>
2023-03-25 19:27:39 -0600

    fixed path to images on locations handlebars

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,17 +1,20 @@
-// const calculateAverageRating = (rating) => {
-//   let ratingArr = [];
-//   ratingArr.push(rating);
-//   forEach(calculateAverageRating(rating));
-//   console.log(ratingArr);
-//   return ratingArr;
-// };
+// // const calculateAverageRating = (rating) => {
+// //   let ratingArr = [];
+// //   ratingArr.push(rating);
+// //   forEach(calculateAverageRating(rating));
+// //   console.log(ratingArr);
+// //   return ratingArr;
+// // };
 
-module.exports = {
-  calculateAverageRating: (rating) => {
-    let ratingArr = [];
-    ratingArr.push(rating);
-    forEach(calculateAverageRating(rating));
-    console.log(ratingArr);
-    return ratingArr;
-  },
-};
+// const { options } = require("../controllers");
+
+// module.exports = {
+//   calculateAverageRating: (rating) => {
+//     console.log("test");
+//     let ratingArr = rating + 10;
+//     // ratingArr.push(rating);
+//     // forEach(calculateAverageRating(rating));
+//     // console.log(ratingArr);
+//     return ratingArr;
+//   },
+// };
