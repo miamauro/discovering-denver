c1555fa932372231854a36144e570a50ecd12c72
James Geneser <jamesgeneser@gmail.com>
2023-03-27 11:26:29 -0600

    validation to write review

--- utils/helpers.js
+++ utils/helpers.js
@@ -8,12 +8,13 @@ module.exports = {
         return a + b;
       });
       let ratingLength = ratingArr.length;
+
       const averageRating = arrSum / ratingLength;
+      return averageRating.fixed(1);
     });
 
     console.log(ratingArr);
     console.log(averageRating);
     // return ratingLength;
-    return averageRating.fixed(1);
   },
 };
