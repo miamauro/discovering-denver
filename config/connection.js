96c8a8eb4ee85d0b39cb860fd854b743f819b69a
Thomas Wray <twray95@gmail.com>
2023-03-27 13:53:27 -0600

    changed host name

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: "127.0.0.1",
+    host: "z3iruaadbwo0iyfp.cbetxkdyhwsb.us",
     dialect: "mysql",
     port: 3306,
   }
