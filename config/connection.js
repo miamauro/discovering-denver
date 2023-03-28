2a4ab3639de7a4a0c722015f1e95984c7cae95a7
Thomas Wray <twray95@gmail.com>
2023-03-28 09:07:33 -0600

    commit

--- config/connection.js
+++ config/connection.js
@@ -14,15 +14,5 @@ const sequelize = process.env.JAWSDB_URL
         port: 3306,
       }
     );
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: "127.0.0.1",
-    dialect: "mysql",
-    port: 3306,
-  }
-);
 
 module.exports = sequelize;
